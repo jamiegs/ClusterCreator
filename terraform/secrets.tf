9c34c44a3fa0909160e64dc1f5c916d52aa0a7c5
Jamie Snell <jamie.snell@hudl.com>
2025-02-10 22:30:11 -0600

    update token

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -4,6 +4,6 @@ locals {
   vm_password       = "DE!!de11"
   vm_ssh_key        = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILka0tLvzeyKxQaDnI3uhUrYpVgqzUoJLMYwYdePXQgk jamie.snell@HUD010769"
   proxmox_username  = "terraform"
-  proxmox_api_token = "terraform@pve!provider=97ef3613-3120-42dd-aab4-aee40f27acd8"
+  proxmox_api_token = "terraform@pve!terraform-token=97ef3613-3120-42dd-aab4-aee40f27acd8"
   cluster_to_build  = "alpha"
 }
